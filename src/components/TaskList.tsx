b69d5abebc183d903a31f4c79fda02fa6e9324b9
Lucas Maciel <lucas.maciel@wisereducacao.com>
2021-03-16 19:54:39 -0300

    feat: todo list crud

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+    
+    const newTask: Task ={
+      id:Number(Date.now()),
+      isComplete:false,
+      title: newTaskTitle
+    }
+
+    setTasks([...tasks,newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskUpdate = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = true
+      }
+      return task
+      
+    })
+    setTasks(taskUpdate)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskUpdate = tasks.filter(task => task.id !== id)
+
+    setTasks(taskUpdate)
   }
 
   return (
